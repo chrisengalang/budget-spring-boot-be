8c1487fc32596e0533f2fa5099f317cc1f51dc7a
Chrisen Galang <chrisengalang@Chrisens-MacBook-Pro.local>
2024-04-25 11:50:13 +0800

    Initial commit with github action

---
+++ src/test/java/dev/chrisen/budget/BudgetSpringBootBeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.chrisen.budget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetSpringBootBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
